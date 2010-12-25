067ebea0aa75d765451be1ecf04e608234b0f47a
Andrew V. Melnichuk <andrew.melnichuk@gmail.com>
2010-12-26 01:38:16 +0200

    upgrade to asp.net mvc3 rc2

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace Yuka.Web.Controllers
 	{
 		public ActionResult Index()
 		{
-			ViewModel.Message = "Welcome to ASP.NET MVC!";
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
 
 			return View();
 		}
